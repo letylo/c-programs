59fd87620f36899f3cddf9bf41918a585bc7da79
ATENEA <atenea@ubuntu.(none)>
2014-02-15 16:13:20 +0100

    Ejercicios C

---
+++ 12_sort/sort.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+
+    const char *list[] = {"yield", "yelp", "yellow"};
+    int contador = strcmp(list[0], list[1]);
+
+    
+    return EXIT_SUCCESS;
+
+}
